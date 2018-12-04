52fcdcb5e10a3ef6b3621abb5dd980c058189709
Stephanie Messad <sabud1208@gmail.com>
2018-12-04 06:11:29 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +13,13 @@ def first_challenge
     }
   }
 
-  #your code here
+
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
+  # data.each do |attribute, value|
+  #   puts "#{attribute}: #{value}"
+  # end
 
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,4 +1,7 @@
+require "pry"
+
 def second_challenge
+
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
    vegetable: ["carrots", "broccoli", "cucumbers"],
@@ -7,7 +10,8 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
+groceries.values.flatten do |food, type|
+  "#{food}: #{type}"
+end
 
 end
